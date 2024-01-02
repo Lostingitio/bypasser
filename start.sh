600c10833feeba14cce17e6b33fe08be3b20cb94
LieStage <118051769+LieStage@users.noreply.github.com>
2024-01-02 07:40:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,7 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/bipinkrish/Link-Bypasser-Bot /Link-Bypasser-Bot
-else
+  $UPSTREAM_REPO="https://github.com/Lostingitio/Link-Bypasser-Bot"
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Link-Bypasser-Bot
-fi
+
 cd /Link-Bypasser-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bypass Bot...."

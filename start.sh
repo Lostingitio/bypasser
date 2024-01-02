ee0181ac30f608a3bc18ceb09b43b9eec46c09bd
LieStage <118051769+LieStage@users.noreply.github.com>
2024-01-02 07:48:06 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-  $UPSTREAM_REPO="https://github.com/Lostingitio/Link-Bypasser-Bot"
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Link-Bypasser-Bot
-
-cd /Link-Bypasser-Bot
-pip3 install -U -r requirements.txt
-echo "Starting Bypass Bot...."
-python3 main.py
